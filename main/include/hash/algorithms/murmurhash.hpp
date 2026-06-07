424b258aa07a9877309e42301f433fe0dabffd14
EricoMeger <ericomeger9@gmail.com>
2026-06-07 19:36:20 -0300

    add hash function for btree key

---
+++ main/include/hash/algorithms/murmurhash.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../hash_function.hpp"
+#include <cstddef>
+#include <cstdint>
+
+class MurmurHash : public HashFunction {
+public:
+	explicit MurmurHash(uint32_t seed = 0);
+
+	uint64_t hash(const std::string& input) override;
+
+	std::string getName() const override {
+		return "MurmurHash";
+	}
+
+private:
+	uint32_t seed;
+};
+
+uint32_t murmur3_32(const void* key, size_t len, uint32_t seed);
